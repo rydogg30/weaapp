5b89b8ee005a4c598171aaf89dc224d6a23ac3a3
rydogg30 <ryancaufieldmail@gmail.com>
2023-04-10 19:46:26 -0600

    Feature:InitialCommit

---
+++ src/pages/contact.js
@@ -0,0 +1,17 @@
+const Contact = () => {
+    return (
+        <div
+            style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100vh'
+            }}
+        >
+           <h1>Contact Us</h1>
+           <hr /> 
+        </div>
+    );
+};
+
+export default Contact;
